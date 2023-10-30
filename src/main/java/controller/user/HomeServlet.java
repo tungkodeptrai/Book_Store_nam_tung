2c064eda2b774144bfdfb44bbe13116d28c443ac
tungdeptrai <thichsao11@gmail.com>
2023-10-30 16:57:58 +0700

    nam luoi hoc

--- src/main/java/controller/user/HomeServlet.java
+++ src/main/java/controller/user/HomeServlet.java
@@ -32,7 +32,7 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        // nam ngu lon
         // set utf8 to searth with vietnam languge
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
